ab7652b9ab57642e1c30fc24ea52b5e75a532d00
jinkyu <s1strato@gmail.com>
2017-05-18 15:45:56 +0100

    merge desc and candidate, qr added

--- js/main.js
+++ js/main.js
@@ -9,30 +9,26 @@
 'use strict';
 
 var localConnection;
-var remoteConnection;
 var sendChannel;
 var receiveChannel;
 var pcConstraint;
 var dataConstraint;
-var dataChannelSend = document.querySelector('textarea#dataChannelSend');
-var dataChannelReceive = document.querySelector('textarea#dataChannelReceive');
-var localDesc = document.querySelector('textarea#localDesc');
-var remoteDesc = document.querySelector('textarea#remoteDesc');
-var startButton = document.querySelector('button#startButton');
-var doneButton = document.querySelector('button#doneButton');
+
 var connectButton = document.querySelector('button#connectButton');
 var sendButton = document.querySelector('button#sendButton');
 var closeButton = document.querySelector('button#closeButton');
-
 var offerSetupButton = document.querySelector('button#offerSetupButton');
 var answerSetupButton = document.querySelector('button#answerSetupButton');
 var offerButton = document.querySelector('button#offerButton');
 var answerButton = document.querySelector('button#answerButton');
+
+var dataChannelSend = document.querySelector('textarea#dataChannelSend');
+var dataChannelReceive = document.querySelector('textarea#dataChannelReceive');
+var localDesc = document.querySelector('textarea#localDesc');
+var remoteDesc = document.querySelector('textarea#remoteDesc');
 var iceCandidateOffer = document.querySelector('textarea#iceCandidateOffer');
 var iceCandidateAnswer = document.querySelector('textarea#iceCandidateAnswer');
 
-//startButton.onclick = createConnection;
-doneButton.onclick = setRemoteDescriptionManual;
 connectButton.onclick = doConnect;
 sendButton.onclick = sendData;
 closeButton.onclick = closeDataChannels;
@@ -42,9 +38,8 @@ answerSetupButton.onclick = answerSetup;
 offerButton.onclick = createOfferManual;
 answerButton.onclick = createAnswerManual;
 
-function enableStartButton() {
-  startButton.disabled = false;
-}
+// mandatories
+var localInfo = {};
 
 function disableSendButton() {
   sendButton.disabled = true;
@@ -56,11 +51,6 @@ function offerSetup() {
   pcConstraint = null;
   dataConstraint = null;
   trace('Using SCTP based data channels');
-  // SCTP is supported from Chrome 31 and is supported in FF.
-  // No need to pass DTLS constraint as it is on by default in Chrome 31.
-  // For SCTP, reliable and ordered is true by default.
-  // Add localConnection to global scope to make it visible
-  // from the browser console.
   window.localConnection = localConnection =
       new RTCPeerConnection(servers, pcConstraint);
   trace('Created local peer connection object localConnection');
@@ -75,6 +65,8 @@ function offerSetup() {
       console.log("localConnection.onicecandidate");
       console.log(e);
       iceCandidateOffer.value += JSON.stringify(e.candidate);
+      localInfo.candidate = e.candidate;
+      makeQRIfReady();
     }
     else
       console.log("");
@@ -92,6 +84,34 @@ function offerSetup() {
   };
   sendChannel.onopen = onSendChannelStateChange;
   sendChannel.onclose = onSendChannelStateChange;
+  createOfferManual();
+}
+
+function createOfferManual() {
+  console.log("create offer");
+  localConnection.createOffer().then(
+    gotLocalDescription,
+    onCreateSessionDescriptionError
+  );
+  closeButton.disabled = false;
+}
+
+function gotLocalDescription(desc) {
+  console.log("localConnection.setLocalDescription");
+  localConnection.setLocalDescription(desc);
+  trace('Offer from localConnection \n' + desc.sdp);
+  console.log(desc);
+
+  localDesc.value = JSON.stringify(desc);
+  localInfo.desc = desc;
+  makeQRIfReady();
+}
+
+function makeQRIfReady() {
+  console.log(localInfo);
+  console.log('string: ' + JSON.stringify(localInfo));
+  console.log('count: ' + JSON.stringify(localInfo).length);
+  document.getElementById("qrimage").innerHTML="<img src='https://chart.googleapis.com/chart?chs=500x500&cht=qr&chl="+encodeURIComponent(JSON.stringify(localInfo))+"'/>";
 }
 
 function answerSetup() {
@@ -101,142 +121,65 @@ function answerSetup() {
   dataConstraint = null;
   trace('Using SCTP based data channels');
 
-  // Add remoteConnection to global scope to make it visible
-  // from the browser console.
-  window.remoteConnection = remoteConnection =
+  window.localConnection = localConnection =
       new RTCPeerConnection(servers, pcConstraint);
-  trace('Created remote peer connection object remoteConnection');
+  trace('Created remote peer connection object localConnection');
 
-  remoteConnection.onicecandidate = function(e) {
+  localConnection.onicecandidate = function(e) {
     if (e.candidate && e.candidate.candidate.indexOf('.') > -1)
     {
-      console.log("remoteConnection.onicecandidate " + e.candidate.candidate.indexOf('.'));
+      console.log("localConnection.onicecandidate " + e.candidate.candidate.indexOf('.'));
       console.log(e);
       iceCandidateOffer.value += JSON.stringify(e.candidate);
+      localInfo.candidate = e.candidate;
+      makeQRIfReady();
     }
     else
       console.log("");
   };
-  remoteConnection.ondatachannel = receiveChannelCallback;
-}
-
-function createOfferManual() {
-  console.log("create offer");
-  localConnection.createOffer().then(
-    gotDescription1,
-    onCreateSessionDescriptionError
-  );
-  startButton.disabled = true;
-  closeButton.disabled = false;
+  localConnection.ondatachannel = receiveChannelCallback;
+  createAnswerManual();
 }
 
 function createAnswerManual() {
-  var desc = JSON.parse(remoteDesc.value);
-  console.log("remoteConnection.setRemoteDescription");
-  remoteConnection.setRemoteDescription(desc);
+  //var desc = JSON.parse(remoteDesc.value);
+  var server = JSON.parse(document.querySelector('textarea#serverInfo').value);
+  var desc = server.desc;
+  console.log("localConnection.setRemoteDescription");
+  localConnection.setRemoteDescription(desc);
   console.log("create answer");
-  remoteConnection.createAnswer().then(
-    gotDescription2,
+  localConnection.createAnswer().then(
+    gotRemoteDescription,
     onCreateSessionDescriptionError
   );
 
-  remoteConnection.addIceCandidate(JSON.parse(iceCandidateAnswer.value));
+  //localConnection.addIceCandidate(JSON.parse(iceCandidateAnswer.value));
+  localConnection.addIceCandidate(server.candidate);
 }
+
+function gotRemoteDescription(desc) {
+  console.log("localConnection.setLocalDescription");
+  localConnection.setLocalDescription(desc);
+  trace('Answer from localConnection \n' + desc);
+  console.log(desc);
+  localDesc.value = JSON.stringify(desc);
+  localInfo.desc = desc;
+  makeQRIfReady();
+}
+
 
 function onCreateSessionDescriptionError(error) {
   trace('Failed to create session description: ' + error.toString());
 }
 
-function sendData() {
-  var data = dataChannelSend.value;
-  sendChannel.send(data);
-  trace('Sent Data: ' + data);
-}
-
-function closeDataChannels() {
-  trace('Closing data channels');
-  sendChannel.close();
-  trace('Closed data channel with label: ' + sendChannel.label);
-  receiveChannel.close();
-  trace('Closed data channel with label: ' + receiveChannel.label);
-  localConnection.close();
-  remoteConnection.close();
-  localConnection = null;
-  remoteConnection = null;
-  trace('Closed peer connections');
-  startButton.disabled = false;
-  sendButton.disabled = true;
-  closeButton.disabled = true;
-  dataChannelSend.value = '';
-  dataChannelReceive.value = '';
-  dataChannelSend.disabled = true;
-  disableSendButton();
-  enableStartButton();
-}
-
-function gotDescription1(desc) {
-  console.log("localConnection.setLocalDescription");
-  localConnection.setLocalDescription(desc);
-  trace('Offer from localConnection \n' + desc.sdp);
-  console.log(desc);
-
-  localDesc.value = JSON.stringify(desc);
-
-/*
-  remoteConnection.setRemoteDescription(desc);
-  remoteConnection.createAnswer().then(
-    gotDescription2,
-    onCreateSessionDescriptionError
-  );
-*/
-}
-
-function setRemoteDescriptionManual() {
-  var desc = JSON.parse(remoteDesc.value);
-  remoteConnection.setRemoteDescription(desc);
-}
-
 function doConnect() {
-  var desc = JSON.parse(remoteDesc.value);
+  //var desc = JSON.parse(remoteDesc.value);
+  var client = JSON.parse(document.querySelector('textarea#clientInfo').value);
+  var desc = client.desc;
   console.log("localConnection.setRemoteDescription");
   localConnection.setRemoteDescription(desc);
-  localConnection.addIceCandidate(JSON.parse(iceCandidateAnswer.value));
-}
-
-function gotDescription2(desc) {
-  console.log("remoteConnection.setLocalDescription");
-  remoteConnection.setLocalDescription(desc);
-  trace('Answer from remoteConnection \n' + desc);
-  console.log(desc);
-  localDesc.value = JSON.stringify(desc);
-//  localConnection.setRemoteDescription(desc);
-}
-
-function getOtherPc(pc) {
-  return (pc === localConnection) ? remoteConnection : localConnection;
-}
-
-function getName(pc) {
-  return (pc === localConnection) ? 'localPeerConnection' :
-      'remotePeerConnection';
-}
-
-function onIceCandidate(pc, event) {
-  /*
-  getOtherPc(pc).addIceCandidate(event.candidate)
-  .then(
-    function() {
-      onAddIceCandidateSuccess(pc);
-    },
-    function(err) {
-      onAddIceCandidateError(pc, err);
-    }
-  );
-  trace(getName(pc) + ' ICE candidate: \n' + (event.candidate ?
-      event.candidate.candidate : '(null)'));
-  trace(event.candidate);
-  trace(event.candidate.candidate);
-  */
+  //localConnection.addIceCandidate(JSON.parse(iceCandidateAnswer.value));
+  localConnection.addIceCandidate(client.candidate);
 }
 
 function onAddIceCandidateSuccess() {
@@ -279,3 +222,29 @@ function onReceiveChannelStateChange() {
   var readyState = receiveChannel.readyState;
   console.log('Receive channel state is: ' + readyState);
 }
+
+function sendData() {
+  var data = dataChannelSend.value;
+  sendChannel.send(data);
+  trace('Sent Data: ' + data);
+}
+
+function closeDataChannels() {
+  trace('Closing data channels');
+  sendChannel.close();
+  trace('Closed data channel with label: ' + sendChannel.label);
+  receiveChannel.close();
+  trace('Closed data channel with label: ' + receiveChannel.label);
+  localConnection.close();
+  localConnection.close();
+  localConnection = null;
+  localConnection = null;
+  trace('Closed peer connections');
+  sendButton.disabled = true;
+  closeButton.disabled = true;
+  dataChannelSend.value = '';
+  dataChannelReceive.value = '';
+  dataChannelSend.disabled = true;
+  disableSendButton();
+}
+
