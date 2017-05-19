29de26c19e4e7ddcf2b4a43d33fa540bee52141f
jinkyu <s1strato@gmail.com>
2017-05-19 15:14:16 +0100

    fix layout for earnings and cfo

--- js/scoreboard.js
+++ js/scoreboard.js
@@ -1,13 +1,13 @@
 'use strict';
 
 var playerCount = 0;
-var bankBalance = 0;
+var bankBalance = 100;
 var playerBalanceElements = [];
 var lastPlayerEarned = [];
 var lastTimeOut;
-var LAST_TIMEOUT = 10000;
+var LAST_TIMEOUT = 3000;
 
-var moneyTypes = [100, 50, 10, 5, 1];
+var moneyTypes = [50, 10, 5, 1];
 
 $('document').ready(onLoad);
 
@@ -20,6 +20,11 @@ function onLoad() {
 }
 
 function createOptionMenu() {
+    $('#bankFrame > div').click(function() {
+        applyEranings(true);
+        updateScores();
+    })
+
     $('#toggleFullScreen').click(function() {
      var isInFullScreen = (document.fullscreenElement && document.fullscreenElement !== null) ||
         (document.webkitFullscreenElement && document.webkitFullscreenElement !== null) ||
@@ -58,6 +63,7 @@ function createOptionMenu() {
         if (bankBalance < 0)
             bankBalance = 0;
         bankBalance += 100;   
+        applyEranings(true);
         updateScores();
     });
 
@@ -87,9 +93,29 @@ function createPlayer() {
 
     var innerPlayerName = $('<tr></tr>').append($('<td></td>').text('player '+playerCount)).addClass('playerName');
 
-    var innerPlayerScore = $('<tr></tr>').append($('<td></td>')).attr('id', 'score'+playerCount);
+    var innerPlayerScore = $('<div></div>').attr('id', 'score'+playerCount);
     innerPlayerScore.attr('value', 0);
-    innerPlayerScore.addClass('scoreStyle');
+//    innerPlayerScore.addClass('scoreStyle');
+    innerPlayerScore.click(function() {
+        console.log('clicked');
+        applyEranings(true);
+    })
+
+    var innerPlayerEarnings = $('<span></span>').attr('id', 'earnings'+playerCount);
+    innerPlayerEarnings.attr('value', 0);
+    innerPlayerEarnings.addClass('scoreStyleSmall');
+    innerPlayerEarnings.click(function() {
+        console.log('clicked');
+        applyEranings(true);
+    })
+
+    var innerPlayerCFO= $('<span></span>').attr('id', 'cfo'+playerCount);
+    innerPlayerCFO.attr('value', 0);
+    innerPlayerCFO.addClass('scoreStyleSmall2');
+    innerPlayerCFO.click(function() {
+        console.log('clicked');
+        applyEranings(true);
+    })
 
     var innerPlayerButtons = $('<td></td>').attr('id', 'btn_'+playerCount).addClass('playerButtons');
 
@@ -107,16 +133,45 @@ function createPlayer() {
         innerPlayerButtons.append(button);
     });
 
-    var check = $('<input></input>')
+    var checkExpense = $('<input></input>')
         .attr('type', 'checkbox')
         .attr('id', 'chk_'+playerCount)
         .click(function() {
             console.log(this.checked);
+            clearTimeout(lastTimeOut);
+            lastTimeOut = setTimeout(function() { applyEranings(false); }, LAST_TIMEOUT);
+            updateScores();
         });
 
-    innerPlayerButtons.append($('<div></div>', '{overflow: hidden;}').addClass('chkStyle').append($('<label>expense</label>').append(check)));
+    innerPlayerButtons.append($('<div></div>', '{overflow: hidden;}').addClass('chkStyle').append($('<label>expense</label>').append(checkExpense)));
 
-    innerPlayerTable.append(innerPlayerName).append(innerPlayerScore).append($('<tr></tr>').append(innerPlayerButtons));
+    var checkCFO = $('<input></input>')
+        .attr('type', 'checkbox')
+        .attr('id', 'cfo_'+playerCount)
+        .click(function() {
+            console.log(this.checked);
+            clearTimeout(lastTimeOut);
+            lastTimeOut = setTimeout(function() { applyEranings(false); }, LAST_TIMEOUT);
+            updateScores();
+        });
+
+    innerPlayerButtons.append($('<div></div>', '{overflow: hidden;}').addClass('chkStyle').append($('<label>CFO</label>').append(checkCFO)));
+
+    innerPlayerTable.append(innerPlayerName)
+    innerPlayerTable
+        .append($('<tr></tr>')
+                    .append($('<td></td>')
+                                .append($('<div></div>')
+                                    .css('overflow', 'hidden')
+                                    .addClass('scoreStyle')
+                                    .append(innerPlayerScore)
+                                    .append($('<div></div>')
+                                        .append(innerPlayerEarnings)
+                                        .append(innerPlayerCFO)
+                                ))));
+
+    innerPlayerTable.append($('<tr></tr>').append(innerPlayerButtons));
+
     $('#playerTable > tbody > tr').append($('<td></td>').append(innerPlayerTable));
 
     playerBalanceElements[playerCount] = innerPlayerScore;
@@ -130,34 +185,63 @@ function playerEarned(playerIndex, amount) {
     if ($('#chk_'+playerIndex).is(':checked'))
     {
         lastPlayerEarned[playerIndex] -= amount;
+        bankBalance += amount;
     }
     else
     {
         lastPlayerEarned[playerIndex] += amount;
+        bankBalance -= amount;
     }
     updateScores();
     clearTimeout(lastTimeOut);
-    lastTimeOut = setTimeout(applyEranings, LAST_TIMEOUT);
+    lastTimeOut = setTimeout(function() { applyEranings(false); }, LAST_TIMEOUT);
 }
 
 function updateScores() {
     playerBalanceElements.forEach(function(elm) {
         var id = parseInt(elm.attr('id').substr(5));
-        elm.text(elm.attr('value') + 
-            ((lastPlayerEarned[id] != 0) ? (' +' + lastPlayerEarned[id]) : ''));
+        elm.text(elm.attr('value'));
     });
-    $('#bankFrame > div').text(bankBalance);
+    var tempLast = 0;
+    for (var i = 0; i < lastPlayerEarned.length; ++i) 
+    {
+        if (lastPlayerEarned[i] == 0)
+        {
+            $('#earnings'+i).hide();
+            $('#cfo'+i).hide();
+        }
+        else
+        {
+            $('#earnings'+i).show();
+            $('#earnings'+i).text(lastPlayerEarned[i]);
+            if ($('#cfo_'+i).is(':checked'))
+            {
+                $('#cfo'+i).show();
+                $('#cfo'+i).text('+' + Math.floor(lastPlayerEarned[i] * .5));
+                tempLast += Math.floor(lastPlayerEarned[i] * .5);
+            }
+            else
+                $('#cfo'+i).hide();
+        }
+    }
+    $('#bankFrame > div').text(bankBalance - tempLast);
 }
 
-function applyEranings() {
+function applyEranings(force) {
+    if (force)
+        clearTimeout(lastTimeOut);
     for (var i = 0; i < lastPlayerEarned.length; ++i) 
     {
         var element = playerBalanceElements[i];
         var balance = parseInt(element.attr('value'));
-        element.attr('value', balance + lastPlayerEarned[i]);
-        bankBalance -= lastPlayerEarned[i];
+        balance += lastPlayerEarned[i];
+        if ($('#cfo_'+i).is(':checked'))
+        {
+            balance += Math.floor(lastPlayerEarned[i] * .5);
+            bankBalance -= Math.floor(lastPlayerEarned[i] * .5);
+        }
+        element.attr('value', balance);
         lastPlayerEarned[i] = 0;
     }
     updateScores();
 }
-
